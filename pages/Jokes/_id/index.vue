932b9c03fca8ac56cff3abbe69a4a56b850827e2
Simon Andersen <simo414u@stud.kea.dk>
2021-08-24 11:55:30 +0200

    Now ading search field

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>{{ $route.params.id }}</div>
+  <div>
+    <nuxt-link to="/jokes">Back to jokes</nuxt-link>
+    <h2>{{ joke }}</h2>
+    <hr />
+    <small>Joke ID: {{ $route.params.id }}</small>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      joke: {},
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+      //   console.log(res.data.joke);
+      this.joke = res.data.joke;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  head() {
+    return {
+      title: "Single joke",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for dumb jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
+
 
 <style>
 </style>
