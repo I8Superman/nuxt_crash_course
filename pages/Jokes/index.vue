b7f23a13fa3aa64087cec64528d13337c90e45ee
Simon Andersen <simo414u@stud.kea.dk>
2021-08-24 11:21:02 +0200

    Initial commit. Most of app already made

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Joke
+      v-for="joke in jokes"
+      :key="joke.id"
+      :id="joke.id"
+      :joke="joke.joke"
+    />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Joke from "../../components/Joke.vue";
+export default {
+  components: {
+    Joke,
+  },
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      // console.log(res);
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  head() {
+    return {
+      title: "The Jokes of Dad",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for dumb jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Welcome to Dadjokes - best not funny jokes evar!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to Dadjokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for dumb jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
